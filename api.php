ae4d46ee22a54e89b8fb98b33773d817b9f44440
LemiSt24 <lennard.strohmeyer@gmail.com>
2016-02-24 19:19:51 +0100

    Build API and import script for static data.

---
+++ api.php
@@ -0,0 +1,74 @@
+<?php
+require_once("config.inc.php");
+
+define('FLOOR', -1);
+define('CEILING', 1);
+
+//Database connection
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_database);
+if ($mysqli->connect_error) {
+  die('Connect Error (' .$mysqli->connect_errno.') '.$mysqli->connect_error);
+}
+
+//Check URL params
+if(!isset($_GET["typename"]) || !isset($_GET["regionid"]))
+  die("Error: Please supply type name and region ID.");
+
+//Just for making sure nobody messes up our database
+$name=$mysqli->real_escape_string($_GET["typename"]);
+$region_id=$mysqli->real_escape_string($_GET["regionid"]); //Gotta love PHP
+
+//Get actual Type ID
+$query = "SELECT id FROM typeids WHERE name='$name'";
+$result = $mysqli->query($query);
+if(!$result)
+  die("Database Error: ".$mysqli->error());
+
+$type_id = $result->fetch_array(MYSQLI_ASSOC)["id"];
+
+//Get blueprint data
+$query = "SELECT * FROM blueprints WHERE id=$type_id";
+$result = $mysqli->query($query);
+if(!$result)
+  die("Database Error: ".$mysqli->error());
+
+$arrBlueprint = $result->fetch_array(MYSQLI_ASSOC);
+$arrMaterials = unserialize(base64_decode($arrBlueprint["materials"]));
+$arrProducts = unserialize(base64_decode($arrBlueprint["products"]));
+
+$return_array = array("name" => $name,
+  "typeID" => $type_id,
+  "price" => get_price($region_id, $type_id, "sell", FLOOR),
+  "mats" => array());
+
+//fill mats array
+foreach($arrMaterials as $material) {
+  $query = "SELECT name FROM typeids WHERE id=$material[typeID]";
+  $result = $mysqli->query($query);
+  if(!$result)
+    die("Database Error: ".$mysqli->error());
+  $mat_name = $result->fetch_array(MYSQLI_ASSOC)["name"];
+
+  $return_array["mats"][] = array("name" => $mat_name, "typeID" => $material["typeID"], get_price($region_id, $type_id, "buy", CEILING));
+}
+
+//Return Array
+echo json_encode($return_array);
+
+//returns lowest or highest ask/bid price for specified Item
+function get_price($region_id, $type_id, $action, $direction) {
+  $request_url="https://public-crest.eveonline.com/market/$region_id/orders/$action/?type=https://public-crest.eveonline.com/types/$type_id/";
+
+  $ret = ($direction == FLOOR) ? INF:0;
+
+  $orders = json_decode(file_get_contents($request_url), true);
+  foreach($orders["items"] as $item) {
+    if($direction == FLOOR && $item["price"] < $ret)
+      $ret = $item["price"];
+    elseif($direction == CEILING && $item["price"] < $ret)
+      $ret = $item["price"];
+  }
+  return $ret;
+}
+
+?>
---
+++ import.php
@@ -0,0 +1,87 @@
+<?php
+require_once("config.inc.php");
+require_once(getcwd()."/spyc/Spyc.php");
+/*
+imports data from SDE (typeIDs, blueprints)
+*/
+
+if(!is_writable(getcwd()))
+  die("Directory not writable, aborting...");
+
+//Database connection
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_database);
+if ($mysqli->connect_error) {
+  die('Connect Error (' .$mysqli->connect_errno.') '.$mysqli->connect_error);
+}
+
+//Create Tables
+echo "Creating Database Tables...\n";
+$query = "CREATE TABLE IF NOT EXISTS blueprints (id INT NOT NULL, materials TEXT NOT NULL default '', products TEXT NOT NULL default '', PRIMARY KEY (id))";
+$result = $mysqli->query($query);
+if(!$result)
+  die("Database error: ".$mysqli->error);
+
+$query = "CREATE TABLE IF NOT EXISTS typeids (id INT NOT NULL, name VARCHAR(255) NOT NULL default '', PRIMARY KEY (id))";
+$result = $mysqli->query($query);
+if(!$result)
+  die("Database error: ".$mysqli->error);
+
+//Download and unpack sources
+echo "Downloading and unpacking sources...\n";
+download_and_unpack($src_blueprint, getcwd()."/blueprints.yaml");
+download_and_unpack($src_typeIDs, getcwd()."/typeids.yaml");
+
+//Import blueprints
+echo "Loading blueprints...\n";
+$blueprints = Spyc::YAMLLoad(getcwd()."/blueprints.yaml");
+echo "Importing blueprints...\n";
+foreach($blueprints as $blueprint) {
+  $id = $blueprint["blueprintTypeID"];
+  //Not the prettiest way to save this type of data, but it suffices in this case
+  $manufacturing = $blueprint["activities"]["manufacturing"];
+  $materials = base64_encode(serialize($manufacturing["materials"]));
+  $products = base64_encode(serialize($manufacturing["products"]));
+
+  $query = "INSERT INTO blueprints(id, materials, products) VALUES($id, '$materials', '$products')";
+  $result = $mysqli->query($query);
+  if(!$result)
+    die("Database Error while importing:".$mysqli->error);
+}
+
+//Free memory
+unset($blueprints);
+
+//Import typeids
+echo "Loading typeIDs...\n";
+$typeids = Spyc::YAMLLoad(getcwd()."/typeids.yaml");
+echo "Importing typeIDs...\n";
+foreach($typeids as $id=>$array) {
+  $name = $array["name"]["en"];
+
+  $query = "INSERT INTO typeids(id, name) VALUES($id, '$name')";
+  $result = $mysqli->query($query);
+  if(!$result)
+    die("Database Error while importing:".$mysqli->error);
+}
+
+echo "All done :)";
+
+$mysqli->close();
+
+function download_and_unpack($source, $destination) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $source);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $data = curl_exec ($ch);
+  $error = curl_error($ch);
+  if($error)
+    die($error);
+  curl_close ($ch);
+
+  $file = fopen($destination, "w+") or die("Could not open $destination. Aborting.");
+  $data = bzdecompress($data);
+  fputs($file, $data);
+  fclose($file);
+}
+
+?>
